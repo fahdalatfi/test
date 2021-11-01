38abd10a5389ca8688ccd169cbb1b237d1daa1ae
Fahd Alatfi <66439678+fahdalatfi@users.noreply.github.com>
2021-11-01 16:17:07 +0200

    Delete main.php
    deleted main.php

--- main.php
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-// added line to hard reset the whole repo
-========
-Alpha edition 
-added line to hard reset the whole repo
->>>>>>> alpha
-hello world 
-This is the file after merging two branches
